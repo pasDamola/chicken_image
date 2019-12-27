f8952b0bf1ce4c66b96e294abaa4bd9725890a67
pasDamola <oyindadavid18@gmail.com>
2019-12-27 02:55:42 -0800

    initial commit

---
+++ main.js
@@ -0,0 +1,49 @@
+for(var i = 0; i < 1; i++){
+    console.log("                                 *  * *    ")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                               *     **  *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                             *          *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                            *          *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                           *         ***")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                          *        **")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                         *           *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("            * * *       *             *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("           *     *     *               *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("          *       * * *                 *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("           *                           *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("            * * *                     *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                 *     *     *  * * *")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                  *   * *   *  ")
+}
+for(var i = 0; i < 1; i++){
+    console.log("                   * *   * *")
+}
+
+for(var i = 0; i < 1; i++){
+    console.log("                    *     *")
+}
